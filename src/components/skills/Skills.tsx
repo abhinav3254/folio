2eb290ffc0343be312fd00735f845026ece8a3f4
abhinav3254 <abhinav3254@gmail.com>
2024-06-01 19:50:55 +0530

    update

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
 import './Skills.css';
 import websiteBuilderSvg from '../../assets/svg/website-img.svg';
 import fullStackSkillsJSON from '../../assets/json/full-stack.json';
-// import '../../assets/skills/fullstack'
+import cloudJSON from '../../assets/json/cloud.json';
+import databaseJSON from '../../assets/json/database.json';
+import mobileJSON from '../../assets/json/mobile.json';
+import serverImg from '../../assets/svg/server.svg';
+import mobileImg from '../../assets/svg/mobile.svg';
+import databaseImg from '../../assets/svg/database.svg';
 
 const Skills = () => {
     return (
@@ -13,13 +17,58 @@ const Skills = () => {
                     <p className='SkillCategory'>Full Stack Development</p>
                     <div className="SkillsContainerRightContainer">
                         {fullStackSkillsJSON.map((data) => (
-                            <div className="ConatinerCard">
+                            <div className="ConatinerCard" key={data.id}>
                                 <img src={require('../../assets/skills/fullstack/' + data.name.toLowerCase() + '.svg')} alt={data.name} />
                                 <p>{data.name}</p>
                             </div>
                         ))}
                     </div>
                 </div>
+            </div>
+
+            <div className="SkillsContainer">
+                <div className="SkillsContainerRight">
+                    <p className='SkillCategory'>Cloud Infra-Architecture</p>
+                    <div className="SkillsContainerRightContainer">
+                        {cloudJSON.map((data) => (
+                            <div className="ConatinerCard" key={data.id}>
+                                <img src={require('../../assets/skills/cloud/' + data.name.toLowerCase() + '.svg')} alt={data.name} />
+                                <p>{data.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <img src={serverImg} alt="" />
+            </div>
+
+            <div className="SkillsContainer">
+                <img src={databaseImg} alt="" />
+                <div className="SkillsContainerRight">
+                    <p className='SkillCategory'>Database</p>
+                    <div className="SkillsContainerRightContainer">
+                        {databaseJSON.map((data) => (
+                            <div className="ConatinerCard" key={data.id}>
+                                <img src={require('../../assets/skills/database/' + data.name.toLowerCase() + '.svg')} alt={data.name} />
+                                <p>{data.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+            <div className="SkillsContainer">
+                <div className="SkillsContainerRight">
+                    <p className='SkillCategory'>Mobile Application Development</p>
+                    <div className="SkillsContainerRightContainer">
+                        {mobileJSON.map((data) => (
+                            <div className="ConatinerCard" key={data.id}>
+                                <img src={require('../../assets/skills/mobile/' + data.name.toLowerCase() + '.svg')} alt={data.name} />
+                                <p>{data.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <img src={mobileImg} alt="" />
             </div>
         </div>
     )
